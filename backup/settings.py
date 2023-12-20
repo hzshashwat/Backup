ebb8332401855a267eb48194d6dfb9d3a76b82e6
Shashwat Singh <shashwatsingh741@gmail.com>
2023-12-20 18:42:10 +0530

    Allowed Hosts set to all

--- backup/settings.py
+++ backup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)prm+(*j8x)w(#2!76==gd07r#c$k=#0dabo$as*&7(zjn$l7_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
